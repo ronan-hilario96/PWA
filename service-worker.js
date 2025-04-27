02a8f4f05de371a402cb33609c5a30cf77bca384
Ronan Hilario Rosa <ronan.hilario96@gmail.com>
2025-04-27 14:38:41 -0300

    Pwa gpt

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,1 @@
-
-self.addEventListener('install', function(e) {
-  console.log('Service Worker instalado.');
-});
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', function(e) { console.log('Service Worker instalado.'); });
